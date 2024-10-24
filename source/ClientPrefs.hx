b175a9d7d387e2ec18c8dd374cb9b41f765ff496
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-10-24 20:53:37 +0700

    init all needed classes, added alot of based save data

---
+++ source/ClientPrefs.hx
@@ -0,0 +1,13 @@
+package;
+
+class ClientPrefs
+{
+	public static var checkUpdated:Bool = true;
+	public static var uiGameplay:String = "Complex";
+	public static var moneyboy:Int = 0;
+	public static var playerLv:Int = 1;
+	public static var playerHP:Int = 20;
+	public static var playerMP:Int = 20;
+	public static var playerItemHave:Array<String> = ["Nothing"];
+	public static var playerName:String = "...";
+}
---
+++ source/TitleSubState.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxSubState;
+
+class TitleSubState extends FlxSubState
+{
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
