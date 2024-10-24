4ead2326f2903c8a456ef7b8d5f649abb549f4d6
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-10-24 21:29:56 +0700

    added credits state

---
+++ source/CreditsState.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxState;
+
+class CreditsState extends FlxState
+{
+	var creditsList:Array<String> = [];
+
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
